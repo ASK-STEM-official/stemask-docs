8bde2a242e3dd7b6191138f2b831deee3f86c5cd
ootomonaiso <ootomonaiso.tubusiki@gmail.com>
2024-08-09 09:41:41 +0900

    feat: Update additionalLanguages in docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -299,7 +299,7 @@ const config: Config = {
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
-      additionalLanguages: ['csharp', 'java', `html`,`css`,`javascript`,`git`],
+      additionalLanguages: ['csharp', 'java'],
     },
   } satisfies Preset.ThemeConfig,
 };
